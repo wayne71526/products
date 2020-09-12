f8ca84c162636e22f92d9d15f1b64ec8c2658c49
Wayne <a0918964526@gmail.com>
2020-09-12 19:52:37 +0800

    輸出成檔案

--- products.py
+++ products.py
@@ -3,7 +3,7 @@ while True:
 	name = input("請輸入商品名稱： ")
 	if name == 'q':
 		break	
-	price = input("請輸入商品價格: ")
+	price = input("請輸入商品價格： ")
 	products.append([name, price])
 print(products)	
 print(products[0][0]) 	
@@ -11,3 +11,7 @@ print(products[0][0])
 for p in products:
 	print(p)
 	print(p[0], '的價格是', p[1])
+
+with open('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
