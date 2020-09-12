03ca3d0dad0d6a30e5287b12ff012e43cacf4c94
Wayne <a0918964526@gmail.com>
2020-09-12 22:14:22 +0800

    add 假查檔案是否存在

--- products.py
+++ products.py
@@ -1,12 +1,17 @@
 # 讀取檔案
 products = []
-with open('products.csv', 'r', encoding = 'utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue #表直接跳到執行下一次迴圈，和break一樣只能用在迴圈
-		name, price = line.strip().split(',') #用split分割後會變清單
-		products.append([name, price])
-print(products)
+import os # 載入作業系統模組
+if os.path.isfile('products.csv'): # 檢查檔案是否存在
+	print('找到檔案了')
+	with open('products.csv', 'r', encoding = 'utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue #表直接跳到執行下一次迴圈，和break一樣只能用在迴圈
+			name, price = line.strip().split(',') #用split分割後會變清單
+			products.append([name, price])
+	print(products)
+else:
+	print('找不到檔案')
 
 # 讓使用者輸入
 while True:
